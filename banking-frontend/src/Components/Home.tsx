0a6c17a928d14bdc2661f7313237a8e3aee6b3ff
JoshM-Yoru <jmaciel335i@gmail.com>
2022-12-19 16:55:32 -0500

    made changes to config

---
+++ banking-frontend/src/Components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+const Container = styled.div`
+`
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            Home
+        </Container>
+    );
+}
+
+export default Home
