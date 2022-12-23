3bc461444ecd8a7c18d65df36baaa314bf8fe265
JoshM-Yoru <jmaciel335i@gmail.com>
2022-12-23 16:46:29 -0500

    adjusted axinstance

--- banking-frontend/src/Util/axInstance.ts
+++ banking-frontend/src/Util/axInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// export const axInst = axios.create({
-//     baseURL: "http://34.229.147.87:8000",
-// });
-
 export const axInst = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://34.229.147.87:8000",
 });
+
+// export const axInst = axios.create({
+//     baseURL: "http://localhost:8000",
+// });
