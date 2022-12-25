1c1af0dbf7a837a13b0d0144b2cf067fc4dc9613
msatin1 <mark@marksatin.com>
2022-12-25 15:42:19 -0500

    styled NavBar to match mock, added conditional logic for user type

--- banking-frontend/src/Components/Navigation/Navbar.tsx
+++ banking-frontend/src/Components/Navigation/Navbar.tsx
@@ -3,26 +3,37 @@ import styled from "styled-components";
 import { UserContext } from "../../Context/UserContext";
 import { UserContextState } from "../../Interfaces/User";
 import { Link } from "react-router-dom";
+import GBLogo_White from "../../Assets/GoodBank-Logo_White.png";
 
 const Container = styled.div`
-    border-bottom: 1px solid ${props => props.theme.border};
     position: sticky;
     top: 0;
-    margin-bottom: 5rem;
+    background: ${props => props.theme.background};
+`;
+const Menu = styled.div`
     display: flex;
-    justify-content: space-between;
-    padding: 1rem;
+    justify-content: flex-end;
+    padding: 0.5rem;
+    padding-right: 1rem;
+    border-bottom: 1px solid ${props => props.theme.border};
 `;
-const Brand = styled.div`
-    & a {
-        text-decoration: none;
-    }
-`;
-const Menu = styled.div``;
+
 const MenuItem = styled.div`
     & a {
         text-decoration: none;
+        color: ${props => props.theme.color};
+        font-weight: bold;
     }
+`;
+const Banner = styled.div`
+    background: ${props => props.theme.primaryDark};
+    box-shadow: 0 10px 5px ${props => props.theme.background};
+    margin-bottom: 0.5rem;
+`;
+const Logo = styled.img`
+    width: 8rem;
+    margin-top: -0.25rem;
+    margin-bottom: -0.5rem;
 `;
 
 const Navbar: React.FC = () => {
@@ -30,21 +41,41 @@ const Navbar: React.FC = () => {
 
     return (
         <Container>
-            <Brand>
-                <Link to={currentUser.type === "" ? "/" : "/home"}>GoodBank</Link>
-            </Brand>
-            <Menu>
-                {currentUser.type === "" && (
+            {currentUser.type === "" && (
+                <Menu>
                     <MenuItem>
                         <Link to="/login">Log In</Link>
                     </MenuItem>
-                )}
-                {currentUser.type === "REP" && (
+                </Menu>
+            )}
+            {currentUser.type === "MEMBER" && (
+                <Menu>
+                    <MenuItem>
+                        <Link to="/about-us">About Us</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link to="/account/summary">Account Summary</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link to="/account/settings">Account Settings</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link to="/logout">Log Out</Link>
+                    </MenuItem>
+                </Menu>
+            )}
+            {currentUser.type === "REP" && (
+                <Menu>
                     <MenuItem>
                         <Link to="/register">Register A New User</Link>
                     </MenuItem>
-                )}
-            </Menu>
+                </Menu>
+            )}
+            <Banner>
+                <Link to={currentUser.type === "" ? "/" : "/home"}>
+                    <Logo src={GBLogo_White} alt="White GoodBank logo with mountains" />
+                </Link>
+            </Banner>
         </Container>
     );
 };
