ebe4cca26279e5f6dff194d3af6a9fe3f56202c4
msatin1 <mark@marksatin.com>
2022-12-25 15:43:09 -0500

    added actual text content

--- banking-frontend/src/Components/WelcomePage/WelcomePage.tsx
+++ banking-frontend/src/Components/WelcomePage/WelcomePage.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import AboutUs from './AboutUs'
-import OurServices from './OurServices'
+import React from "react";
+import styled from "styled-components";
+import AboutUs from "./AboutUs";
+import OurServices from "./OurServices";
 
 const Container = styled.div`
     height: 100%;
-`
+`;
 const Wrapper = styled.div`
     height: 100%;
     width: 100%;
-`
+`;
 const TopWrapper = styled.div`
     width: 100%;
     height: 50%;
     display: flex;
-`
+`;
 const WelcomeWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -24,40 +24,40 @@ const WelcomeWrapper = styled.div`
     height: 100%;
     justify-content: center;
     flex: 1;
-`
+`;
 const WelcomeTitle = styled.h1`
     letter-spacing: 2px;
     font-size: 2.5em;
     margin-bottom: 15px;
-`
+`;
 const WelcomeParagraph = styled.p`
     letter-spacing: 1px;
-    margin-bottom: 10px; 
+    margin-bottom: 10px;
     width: 100%;
     font-size: 1.2em;
-`
+`;
 const ImageWrapper = styled.div`
     width: 50%;
     height: fit-content;
     display: flex;
     justify-content: center;
     padding-top: 2%;
-`
+`;
 const Image = styled.img`
     width: 50%;
     height: auto;
-`
+`;
 const BottomWrapper = styled.div`
     width: 100%;
     height: 50%;
     display: flex;
-`
+`;
 const ServicesWrapper = styled.div`
     width: 50%;
-`
+`;
 const AboutWrapper = styled.div`
     width: 50%;
-`
+`;
 
 const WelcomePage: React.FC = () => {
     return (
@@ -65,16 +65,21 @@ const WelcomePage: React.FC = () => {
             <Wrapper>
                 <TopWrapper>
                     <WelcomeWrapper>
-                        <WelcomeTitle>Welcome to BANK NAME!</WelcomeTitle>
+                        <WelcomeTitle>Welcome to GoodBank!</WelcomeTitle>
                         <WelcomeParagraph>
                             We're more than just any other bank, we're family helping families!
                         </WelcomeParagraph>
                         <WelcomeParagraph>
-                            Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+                            We are glad you are here. At GoodBank, we pride ourselves on offering
+                            top-quality financial services to our customers. Whether you are looking
+                            for a checking account, a credit card, a loan, or investment
+                            opportunities, we have something for everyone. Our team of dedicated
+                            professionals is here to help you achieve your financial goals. Thank
+                            you for choosing GoodBank. We look forward to serving you.
                         </WelcomeParagraph>
                     </WelcomeWrapper>
                     <ImageWrapper>
-                        <Image src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/9108/bank-clipart-md.png' />
+                        <Image src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/9108/bank-clipart-md.png" />
                     </ImageWrapper>
                 </TopWrapper>
                 <BottomWrapper>
@@ -87,7 +92,7 @@ const WelcomePage: React.FC = () => {
                 </BottomWrapper>
             </Wrapper>
         </Container>
-    )
-}
+    );
+};
 
-export default WelcomePage
+export default WelcomePage;
