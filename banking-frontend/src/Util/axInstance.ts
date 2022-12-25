d6356b91406b5133525bc8ee8f5634e58f297140
msatin1 <mark@marksatin.com>
2022-12-25 14:12:02 -0500

    minor code refactor

--- banking-frontend/src/Util/axInstance.ts
+++ banking-frontend/src/Util/axInstance.ts
@@ -2,8 +2,5 @@ import axios from "axios";
 
 export const axInst = axios.create({
     baseURL: "http://34.229.147.87:8000",
+    // baseURL: "http://localhost:8000",
 });
-
-// export const axInst = axios.create({
-//     baseURL: "http://localhost:8000",
-// });
