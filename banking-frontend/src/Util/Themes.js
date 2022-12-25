4d3c67aa76841fb040ee0bb610554e4cc407b952
msatin1 <mark@marksatin.com>
2022-12-25 14:09:36 -0500

    added variable declarations for theme colors

---
+++ banking-frontend/src/Util/Themes.js
@@ -0,0 +1,23 @@
+export const vars = {
+    primaryDark: "#154481",
+    primaryMed: "#577DBE",
+    primaryLight: "C8D5F5",
+};
+
+export const lightTheme = {
+    body: "white",
+    text: "black",
+    background: "#eeeeee",
+    border: "#ccc",
+    altColor: "#333",
+    gradient: "background: linear-gradient(#fff, #888)",
+};
+
+export const darkTheme = {
+    body: "#333",
+    text: "white",
+    background: "#555555",
+    border: "#222",
+    altColor: "#888",
+    gradient: "background: linear-gradient(#333, #fff)",
+};
