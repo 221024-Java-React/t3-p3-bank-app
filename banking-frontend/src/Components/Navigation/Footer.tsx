547e97a2e6e739bf13f806ab38ab57cdb6ed2529
msatin1 <mark@marksatin.com>
2022-12-25 15:41:56 -0500

    styled Footer to match mock

--- banking-frontend/src/Components/Navigation/Footer.tsx
+++ banking-frontend/src/Components/Navigation/Footer.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import GBLogo_Black from "../../Assets/GoodBank-Logo_Black.png";
 
 const Container = styled.div`
     position: sticky;
     bottom: 0;
-    margin-top: 5rem;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    padding: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    border: 2px solid ${props => props.theme.border};
+    border-radius: 5px;
+    background: ${props => props.theme.background};
+    box-shadow: 0 -20px 0px ${props => props.theme.background};
+`;
+const Box = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const Data = styled.p`
+    margin: 0.15rem;
+    font-weight: bold;
+`;
+const Logo = styled.img`
+    width: 10rem;
 `;
 
 const Footer = () => {
-    return <Container>Footer</Container>;
+    return (
+        <Container>
+            <Box>
+                <Data>Oakdale, NY</Data>
+                <Data>Meadowbrook, PA</Data>
+                <Data>Silverlake, NJ</Data>
+            </Box>
+            <Link to="/">
+                <Logo src={GBLogo_Black} alt="Black GoodBank logo with mountains" />
+            </Link>
+            <Box>
+                <Data>(###)-###-####</Data>
+            </Box>
+        </Container>
+    );
 };
 
 export default Footer;
