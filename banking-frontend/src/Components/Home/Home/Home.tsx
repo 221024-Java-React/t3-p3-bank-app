516c974699abc5f5b9154448ccfea242f9a014de
JoshM-Yoru <jmaciel335i@gmail.com>
2022-12-27 17:15:15 -0500

    added changes to welcome page and added react icons

--- banking-frontend/src/Components/Home/Home/Home.tsx
+++ banking-frontend/src/Components/Home/Home/Home.tsx
@@ -12,42 +12,46 @@ import { axInst } from "../../../Util/axInstance";
 const Container = styled.div``;
 
 const Home: React.FC = () => {
-    const { setCurrentAccount } = useContext(AccountContext) as AccountContextState;
-    const { setCurrentUser, currentUser } = useContext(UserContext) as UserContextState;
+    const { setCurrentAccount } = useContext(
+        AccountContext
+    ) as AccountContextState;
+    const { setCurrentUser, currentUser } = useContext(
+        UserContext
+    ) as UserContextState;
 
     const [loading, setLoading] = useState<boolean>(true);
 
     const getAccounts = async () => {
         try {
-            console.log(currentUser.userId)
-            const { data } = await axInst.post<Account[]>(
-                "/accounts/account",
-                { userId: currentUser.userId },
-            )
+            console.log(currentUser.userId);
+            const { data } = await axInst.post<Account[]>("/accounts/account", {
+                userId: currentUser.userId,
+            });
             // console.log(accounts, "accounts");
             // console.log(currentUser, "before setting currentuser account");
             setCurrentUser({ ...currentUser, accounts: data });
             console.log(currentUser, "after setting currentuser account");
             setLoading(false);
             return;
-        } catch (e) {
-        }
-    }
+        } catch (e) { }
+    };
 
     useEffect(() => {
         getAccounts();
     }, []);
 
     if (loading) {
-        return <Container />
+        return <Container />;
     }
-
 
     return (
         <Container>
-            {currentUser.accounts.map(a => {
+            {currentUser.accounts.map((a) => {
                 return (
-                    <Link to={`/${a.type}`} onClick={() => setCurrentAccount(a)}>
+                    <Link
+                        to={`/${a.type}`}
+                        onClick={() => setCurrentAccount(a)}
+                    >
                         <div>{a.type}</div>
                         <div>{a.accountId}</div>
                         <div>{a.balance}</div>
--- banking-frontend/src/Components/WelcomePage/AboutUs.tsx
+++ banking-frontend/src/Components/WelcomePage/AboutUs.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    height: 100%;
-    padding-block: 40px;
-`
+    height: fit-content;
+    padding-top: 40px;
+`;
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
-`
+`;
 const AboutTitle = styled.h2`
     letter-spacing: 2px;
     font-size: 2em;
     padding-left: 5%;
     padding-bottom: 20px;
     height: fit-content;
-`
+`;
 const InfoWrapper = styled.div`
     display: flex;
     height: 100%;
-`
+`;
 const InfoTitle = styled.div`
     width: 75%;
     padding-bottom: 5px;
     font-weight: bold;
-`
+`;
 const ListWrapper = styled.div`
     height: fit-content;
     flex: 1;
@@ -33,20 +33,20 @@ const ListWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-`
+`;
 const InfoFirst = styled.div`
     padding: 8px 12px;
     width: 75%;
     border: 1px solid ${(props) => props.theme.border};
     border-radius: 5px 5px 0 0;
-`
+`;
 const Info = styled.div`
     padding: 8px 12px;
     width: 75%;
     border-bottom: 1px solid ${(props) => props.theme.border};
     border-left: 1px solid ${(props) => props.theme.border};
     border-right: 1px solid ${(props) => props.theme.border};
-`
+`;
 const InfoLast = styled.div`
     padding: 8px 12px;
     width: 75%;
@@ -54,20 +54,16 @@ const InfoLast = styled.div`
     border-left: 1px solid ${(props) => props.theme.border};
     border-right: 1px solid ${(props) => props.theme.border};
     border-radius: 0 0 5px 5px;
-`
+`;
 
 const AboutUs = () => {
     return (
         <Container>
             <Wrapper>
-                <AboutTitle>
-                    About Us
-                </AboutTitle>
+                <AboutTitle>About Us</AboutTitle>
                 <InfoWrapper>
                     <ListWrapper>
-                        <InfoTitle>
-                            Hours:
-                        </InfoTitle>
+                        <InfoTitle>Hours:</InfoTitle>
                         <InfoFirst>Monday: 10am - 5pm</InfoFirst>
                         <Info>Tuesday: 10am - 5pm</Info>
                         <Info>Wednesday: 10am - 5pm</Info>
@@ -76,9 +72,7 @@ const AboutUs = () => {
                         <InfoLast>Saturday: 10am - 3pm</InfoLast>
                     </ListWrapper>
                     <ListWrapper>
-                        <InfoTitle>
-                            Locations:
-                        </InfoTitle>
+                        <InfoTitle>Locations:</InfoTitle>
                         <InfoFirst>Oakdale, New York</InfoFirst>
                         <Info>MeadowBrook, Pennsylvania</Info>
                         <InfoLast>Silverlake, New Jersey</InfoLast>
@@ -86,7 +80,7 @@ const AboutUs = () => {
                 </InfoWrapper>
             </Wrapper>
         </Container>
-    )
-}
+    );
+};
 
-export default AboutUs
+export default AboutUs;
