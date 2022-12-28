57739f7cdc1cbc46b3c9d735acbb6cd9acca252e
hansa98 <hansa@vt.edu>
2022-12-28 15:19:29 -0500

    please disregard

--- banking-backend/src/main/java/com/banking/app/services/CreditCardService.java
+++ banking-backend/src/main/java/com/banking/app/services/CreditCardService.java
@@ -1,42 +1,35 @@
 package com.banking.app.services;
 
-import java.time.LocalDate;
-
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.banking.app.models.User;
-import com.banking.app.repositories.UserRepository;
 import com.banking.app.models.CreditCard;
 import com.banking.app.repositories.CreditCardRepository;
-import com.banking.app.repositories.TransactionRepository;
-
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CreditCardService {
-	private UserRepository uRepo;
 	private CreditCardRepository cRepo;
 	
-	CreditCard createCreditCard(CreditCard c) {
+	public CreditCard createCreditCard(CreditCard c) {
 		Long randLogId = (long) ((Math.random()*(9999999999999999l - 1111111111111111l))+ 1111111111111111l);
 		c.setCardId(randLogId);
 		return cRepo.save(c);
 	}
 	
-	CreditCard getCreditCardByUser(User u) {
+	public CreditCard getCreditCardByUser(User u) {
 	    return cRepo.getCreditCardByUser(u);
 	}
 	
-	CreditCard getCreditCardByAccountId(UUID id) {
+	public CreditCard getCreditCardByAccountId(Long id) {
 	    return cRepo.getCreditCardByCardId(id);
 	}
 	
-	boolean addToCreditCardBalance(Long id, double amount) {
+	public boolean addToCreditCardBalance(Long id, double amount) {
 		CreditCard card = cRepo.getCreditCardByCardId(id);
 		if((card.getBalance()+amount)>card.getCreditLimit()) {
 			card.setBalance(card.getBalance()+amount);
@@ -48,7 +41,7 @@ public class CreditCardService {
 		}
 	}
 	
-	double payCreditCardBalance(UUID id, double amount) {
+	public double payCreditCardBalance(Long id, double amount) {
 		CreditCard card = cRepo.getCreditCardByCardId(id);
 		card.setBalance(card.getBalance()-amount);
 		cRepo.save(card);
