dab835b267e2f881e724a1101e372d490af51201
Dtech7 <neild.felix@gmail.com>
2022-12-28 14:29:45 -0500

    update messageSender, accountCountroller, create limitCalculator

--- banking-backend/src/main/java/com/banking/app/utils/MessageSender.java
+++ banking-backend/src/main/java/com/banking/app/utils/MessageSender.java
@@ -1,6 +1,8 @@
 package com.banking.app.utils;
 
+import com.banking.app.exceptions.CannotUpdateUserException;
 import com.banking.app.models.User;
+import com.banking.app.repositories.UserRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 //import com.twilio.type.PhoneNumber;
@@ -8,13 +10,21 @@ import com.twilio.rest.api.v2010.account.Message;
 public class MessageSender {
     // Find your Account SID and Auth Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
-    public static final String ACCOUNT_SID = System.getenv("TwilioSID");
-    public static final String AUTH_TOKEN = System.getenv("TwilioAuthToken");
+    private static final String ACCOUNT_SID = System.getenv("TwilioSID");
+    private static final String AUTH_TOKEN = System.getenv("TwilioAuthToken");
+    private static UserRepository uRepo;
 
     public static void SendMessage(User u) {
     Integer randLogId = (int) ((Math.random()*(999999 - 111111))+111111);
     	if(u.getAuthToken().equals(null)) {
     		u.setAuthToken(randLogId);
+    		try {
+    			@SuppressWarnings("unused")
+    			User updatedUser = uRepo.save(u);
+    		}catch(Exception e) {
+    			throw new CannotUpdateUserException();
+    		}
+    		
             Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
             Message message = Message.creator(
                     new com.twilio.type.PhoneNumber("+1" + u.getPhoneNumber()),
@@ -25,6 +35,17 @@ public class MessageSender {
             System.out.println(message.getSid());
     		
     	}
-
     }
+    	
+    public static void SendFirstPassword(User u) {
+    	String firstPass = u.getPassword();
+    	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+1" + u.getPhoneNumber()),
+                new com.twilio.type.PhoneNumber(System.getenv("TwilioNumber")),
+                "Verification Number: " + firstPass)
+            .create();
+        System.out.println(message.getSid());
+    }
+  
 }
