c2e1c8b20a94b1dbb08c64f83b93bb2508ed76c0
hansa98 <hansa@vt.edu>
2022-12-28 12:19:18 -0500

    dec28 12:18pm setup creditcard and creditcardapp controllers

--- banking-backend/src/main/java/com/banking/app/repositories/CreditCardRepository.java
+++ banking-backend/src/main/java/com/banking/app/repositories/CreditCardRepository.java
@@ -1,17 +1,12 @@
 package com.banking.app.repositories;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.banking.app.models.User;
-import com.banking.app.models.Account;
 import com.banking.app.models.CreditCard;
-import com.banking.app.models.TransactionData;
-import com.banking.app.models.TransactionType;
 
 @Repository
 public interface CreditCardRepository  extends JpaRepository<CreditCard, Integer> {
