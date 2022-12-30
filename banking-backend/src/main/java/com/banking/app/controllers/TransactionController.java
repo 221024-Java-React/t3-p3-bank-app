1ac0fc815026dd67d1428b16b49ffcc3d06d4a3f
msatin1 <mark@marksatin.com>
2022-12-30 14:53:49 -0500

    removed System out's

--- banking-backend/src/main/java/com/banking/app/controllers/TransactionController.java
+++ banking-backend/src/main/java/com/banking/app/controllers/TransactionController.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,20 @@ public class TransactionController {
 	
 	private TransactionDataService tdServ;
 	
+	@GetMapping("/")
+	public List<TransactionData> getTransactionsByAccountId(UUID accountId) {
+		return tdServ.getTransactionsByAccountId(accountId);
+	}
 	
-	  @GetMapping("/") public List<TransactionData> getTransactionsByAccountId(UUID
-	  accountId) { return tdServ.getTransactionsByAccountId(accountId); }
+	@PostMapping("/transaction/create")
+	public TransactionData addTransactionToAccount(TransactionData td) {
+		return tdServ.createTransaction(td);
+	}
+	
+	@DeleteMapping("/transaction/delete")
+	public TransactionData deleteTransactionInAccount(Integer transactionId) {
+		return tdServ.deleteTransaction(transactionId);
+	}
 	 
 }
+;
